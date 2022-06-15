df95534196fd92e63a41dc148fbd252a43e8d61d
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2022-06-15 08:25:06 +0100

    MMCA-3488 updated library dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,16 +3,16 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.16.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "1.25.0-play-28",
-    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.10.0-play-28",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.24.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.21.0-play-28",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.11.0-play-28",
     "org.typelevel" %% "cats-core" % "2.3.0",
-    "uk.gov.hmrc" %% "mongo-caching" % "7.0.0-play-28",
-    "uk.gov.hmrc" %% "emailaddress" % "3.5.0"
+    "uk.gov.hmrc" %% "mongo-caching" % "7.2.0-play-28",
+    "uk.gov.hmrc" %% "emailaddress" % "3.6.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.16.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.24.0" % Test,
     "org.jsoup" % "jsoup" % "1.13.1" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
     "org.mockito" %% "mockito-scala-scalatest" % "1.16.46" % "test, it"
