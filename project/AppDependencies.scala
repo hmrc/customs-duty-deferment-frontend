5c6d73abed8ddb207b92d36d8ac69eb3ac0316f8
bsgangapat <106598990+bsgangapat@users.noreply.github.com>
2023-03-14 16:30:29 +0000

    MMCA-3859 add play frontend hmrc lib

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.24.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.21.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "6.8.0-play-28",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.11.0-play-28",
     "org.typelevel" %% "cats-core" % "2.3.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.73.0",
