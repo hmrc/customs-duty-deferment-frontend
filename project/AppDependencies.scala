5e965ee1ef489d3324fb9070928b9670e9c71936
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-07-12 16:56:18 +0100

    MMCA-4101 : Update version & configuration for CDS Financials service - July - V1 (#89)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,19 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.15.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.7.0-play-28",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.19.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "org.typelevel" %% "cats-core" % "2.3.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.2.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
     "uk.gov.hmrc" %% "emailaddress" % "3.8.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.15.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.19.0" % Test,
     "org.jsoup" % "jsoup" % "1.13.1" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
-    "org.mockito" %% "mockito-scala-scalatest" % "1.16.46" % "test, it"
+    "org.mockito" %% "mockito-scala-scalatest" % "1.16.46" % "test, it",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0"
   )
 }
