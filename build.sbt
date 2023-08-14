7c8c05c5b1a61a074f35d70ba2cabcf47f00a2a7
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2023-08-14 12:47:23 +0100

    [MMCA-4173] Upgrade version for Non-HMRC dependencies (#91)

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin.publishingSettings
 
 val appName = "customs-duty-deferment-frontend"
 
-val silencerVersion = "1.7.12"
+val silencerVersion = "1.17.13"
 
 lazy val microservice = Project(appName, file("."))
   .enablePlugins(play.sbt.PlayScala, SbtDistributablesPlugin)
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,16 +6,16 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.19.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.13.0-play-28",
-    "org.typelevel" %% "cats-core" % "2.3.0",
+    "org.typelevel" %% "cats-core" % "2.9.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
     "uk.gov.hmrc" %% "emailaddress" % "3.8.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.19.0" % Test,
-    "org.jsoup" % "jsoup" % "1.13.1" % Test,
+    "org.jsoup" % "jsoup" % "1.16.1" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
-    "org.mockito" %% "mockito-scala-scalatest" % "1.16.46" % "test, it",
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.14" % "test, it",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0"
   )
 }
