0bdedd583dc4fa5aa5717a424eb200eaedbf1c19
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2023-09-12 09:21:49 +0100

    [MMCA-4257] Update version & configuration for CDS Financials service (#101)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,9 +2,11 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "7.22.0"
+
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.19.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.19.0-play-28",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.13.0-play-28",
     "org.typelevel" %% "cats-core" % "2.9.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
@@ -12,7 +14,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.19.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % Test,
     "org.jsoup" % "jsoup" % "1.16.1" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
     "org.mockito" %% "mockito-scala-scalatest" % "1.17.14" % "test, it",
