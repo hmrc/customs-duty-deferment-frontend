354bac88052e108913c6ffae4a011075cc771242
HariHmrc <149591728+HariHmrc@users.noreply.github.com>
2024-02-23 15:52:37 +0000

    MMCA-4448 : Fixed recursive depth max error and missed cases error

--- app/models/models.scala
+++ app/models/models.scala
@@ -139,6 +139,7 @@ package object models {
                 }
               }
           }
+        case ( _, _) => JsError(s"Unknown Error on ${path.path} for the value $jsValue")
       }
     }
   }
