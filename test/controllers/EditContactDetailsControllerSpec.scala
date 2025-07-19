cb416b0cfce5f16b0ba68568060304071d184467
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2025-07-20 00:33:22 +0100

    [MMCA-5474] Add more test scenario for EditContactDetailsController

--- test/controllers/EditContactDetailsControllerSpec.scala
+++ test/controllers/EditContactDetailsControllerSpec.scala
@@ -17,18 +17,27 @@
 package controllers
 
 import cache.UserAnswersCache
+import connectors.CustomsFinancialsApiConnector
 import mappings.EditContactDetailsFormProvider
-import models.{EditContactDetailsUserAnswers, UserAnswers}
+import models.responses.retrieve.ContactDetails
+import models.{
+  AccountStatusOpen, DutyDefermentAccountLink, EditContactDetailsUserAnswers, UpdateContactDetailsResponse, UserAnswers
+}
+import org.mockito.Mockito.when
 import pages.EditContactDetailsPage
-import play.api.Application
+import play.api.{Application, inject}
 import play.api.data.Form
 import play.api.i18n.{Messages, MessagesApi}
 import play.api.mvc.{AnyContentAsEmpty, AnyContentAsFormUrlEncoded}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
+import services.{AccountLinkCacheService, ContactDetailsCacheService}
 import util.SpecBase
 import util.TestImplicits.RemoveCsrf
 import views.html.contact_details.edit_contact_details
+import org.mockito.ArgumentMatchers.any
+
+import scala.concurrent.Future
 
 class EditContactDetailsControllerSpec extends SpecBase {
 
@@ -61,6 +70,7 @@ class EditContactDetailsControllerSpec extends SpecBase {
     "return BAD_REQUEST when form errors occur" in new Setup {
       running(appWithUserAnswers) {
         val result = route(appWithUserAnswers, invalidSubmitRequest).value
+
         status(result) mustBe BAD_REQUEST
       }
     }
@@ -68,8 +78,89 @@ class EditContactDetailsControllerSpec extends SpecBase {
     "return a redirect to session expired when user answers is empty" in new Setup {
       running(appWithEmptyUserAnswers) {
         val result = route(appWithEmptyUserAnswers, validSubmitRequest).value
+
         status(result) mustBe SEE_OTHER
         redirectLocation(result).value mustBe routes.SessionExpiredController.onPageLoad.url
+      }
+    }
+
+    "return SEE_OTHER for the valid data" in new Setup {
+
+      val ddAccLink: DutyDefermentAccountLink = DutyDefermentAccountLink(
+        eori = validEori,
+        dan = validDan,
+        linkId = testLinkUrl,
+        status = AccountStatusOpen,
+        statusId = validStatus,
+        isNiAccount = false
+      )
+
+      val contactDetails: ContactDetails = ContactDetails(
+        Some("Mr First Name"),
+        "Example Road",
+        Some("Townsville"),
+        Some("West County"),
+        Some("London"),
+        Some("AA00 0AA"),
+        "GB",
+        Some("+44444111111"),
+        Some("+55555222222"),
+        Some("example@email.com")
+      )
+
+      when(mockAccountLinkCacheService.get(any)).thenReturn(Future.successful(Some(ddAccLink)))
+      when(mockUserAnswersCache.store(any, any)(any)).thenReturn(Future.successful(true))
+      when(mockContactDetailsCache.getContactDetails(any, any, any)(any)).thenReturn(Future.successful(contactDetails))
+      when(mockCustomsFinancialsConnector.updateContactDetails(any, any, any, any)(any))
+        .thenReturn(Future.successful(UpdateContactDetailsResponse(true)))
+      when(mockContactDetailsCache.updateContactDetails(any)(any)).thenReturn(Future.successful(true))
+
+      running(appLinkService(userAnswers)) {
+        val result = route(appLinkService(userAnswers), validSubmitRequest).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe routes.ConfirmContactDetailsController.successContactDetails().url
+      }
+    }
+
+    "redirect to problem page when error occurs while processing the valid data" in new Setup {
+
+      val ddAccLink: DutyDefermentAccountLink = DutyDefermentAccountLink(
+        eori = validEori,
+        dan = validDan,
+        linkId = testLinkUrl,
+        status = AccountStatusOpen,
+        statusId = validStatus,
+        isNiAccount = false
+      )
+
+      val contactDetails: ContactDetails = ContactDetails(
+        Some("Mr First Name"),
+        "Example Road",
+        Some("Townsville"),
+        Some("West County"),
+        Some("London"),
+        Some("AA00 0AA"),
+        "GB",
+        Some("+44444111111"),
+        Some("+55555222222"),
+        Some("example@email.com")
+      )
+
+      when(mockAccountLinkCacheService.get(any)).thenReturn(Future.successful(Some(ddAccLink)))
+      when(mockUserAnswersCache.store(any, any)(any)).thenReturn(Future.successful(true))
+      when(mockContactDetailsCache.getContactDetails(any, any, any)(any))
+        .thenReturn(Future.failed(new RuntimeException("Error occurred")))
+
+      when(mockCustomsFinancialsConnector.updateContactDetails(any, any, any, any)(any))
+        .thenReturn(Future.successful(UpdateContactDetailsResponse(true)))
+      when(mockContactDetailsCache.updateContactDetails(any)(any)).thenReturn(Future.successful(true))
+
+      running(appLinkService(userAnswers)) {
+        val result = route(appLinkService(userAnswers), validSubmitRequest).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe routes.ConfirmContactDetailsController.problem.url
       }
     }
   }
@@ -97,10 +188,24 @@ class EditContactDetailsControllerSpec extends SpecBase {
           ("dan", validDan)
         )
 
-    val mockUserAnswersCache: UserAnswersCache = mock[UserAnswersCache]
-    val appWithUserAnswers: Application        = application(Some(userAnswers))
-    val appWithEmptyUserAnswers: Application   = application(Some(emptyUserAnswers))
-    val appWithoutUsersAnswers: Application    = application
+    val mockUserAnswersCache: UserAnswersCache                        = mock[UserAnswersCache]
+    val mockAccountLinkCacheService: AccountLinkCacheService          = mock[AccountLinkCacheService]
+    val mockCustomsFinancialsConnector: CustomsFinancialsApiConnector = mock[CustomsFinancialsApiConnector]
+    val mockContactDetailsCache: ContactDetailsCacheService           = mock[ContactDetailsCacheService]
+
+    val appWithUserAnswers: Application      = application(Some(userAnswers))
+    val appWithEmptyUserAnswers: Application = application(Some(emptyUserAnswers))
+    val appWithoutUsersAnswers: Application  = application
+
+    def appLinkService(userAnswersInput: UserAnswers = userAnswers): Application =
+      applicationBuilder(Option(userAnswersInput))
+        .overrides(
+          inject.bind[UserAnswersCache].toInstance(mockUserAnswersCache),
+          inject.bind[AccountLinkCacheService].toInstance(mockAccountLinkCacheService),
+          inject.bind[CustomsFinancialsApiConnector].toInstance(mockCustomsFinancialsConnector),
+          inject.bind[ContactDetailsCacheService].toInstance(mockContactDetailsCache)
+        )
+        .build()
 
     val messagesApi: MessagesApi = application(Some(userAnswers)).injector.instanceOf[MessagesApi]
     val messages: Messages       = messagesApi.preferred(onPageLoadRequest)
