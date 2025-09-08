eb14f936c8ee2ffc36c6a431b830d0e7bff01a16
tomndennis <tomndennis@users.noreply.github.com>
2025-09-08 11:15:06 +0100

    DCWL-3720 - Dependency Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion         = "9.19.0"
+  val bootstrapVersion         = "10.1.0"
   private val hmrcMongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
