060fd6a6c5b461d55c176f1e586e9670f1a525ca
Navneet <105457798+tech4nav@users.noreply.github.com>
2026-01-30 11:47:51 +0000

    DCWL-3729 addressed PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion         = "10.1.0"
-  private val hmrcMongoVersion = "2.7.0"
+  val bootstrapVersion         = "10.5.0"
+  private val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.8.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.29.0",
     "org.typelevel"     %% "cats-core"                             % "2.10.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion
   )
