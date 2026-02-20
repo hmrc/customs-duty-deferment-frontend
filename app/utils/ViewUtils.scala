034d998f9d3b526deed3f13e62d0eb5853a63d70
Haseem Isaac <95173192+haseemtasheel@users.noreply.github.com>
2026-02-20 09:35:15 +0000

    DCWL-3730: Headings to Descriptive Lists

--- app/utils/ViewUtils.scala
+++ app/utils/ViewUtils.scala
@@ -21,7 +21,7 @@ import play.twirl.api.{Html, HtmlFormat}
 import uk.gov.hmrc.hmrcfrontend.views.html.components.HmrcNewTabLink
 import uk.gov.hmrc.hmrcfrontend.views.viewmodels.newtablink.NewTabLink
 import utils.Utils.emptyString
-import views.html.components.{caption, h1, h2, inset, link, p}
+import views.html.components.{caption, h1, h2, inset, link, p, dlComponent}
 
 object ViewUtils {
 
@@ -67,6 +67,9 @@ object ViewUtils {
 
   def captionComponent(msg: String, id: Option[String] = None, classes: String): HtmlFormat.Appendable =
     new caption().apply(msg = msg, id = id, classes = classes)
+
+  def dlComponent(dtMsg: String, ddMsg: String, id: Option[String] = None, classes: String = "govuk-caption-xl"): HtmlFormat.Appendable =
+    new dlComponent().apply(dtMsg = dtMsg, ddMsg = ddMsg, id = id, classes = classes)
 
   def hmrcNewTabLinkComponent(
     text: String = emptyString,
