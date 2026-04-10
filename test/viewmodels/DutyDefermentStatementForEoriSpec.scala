683ce44c9a02babdb4cb846af49cf8981ac0f922
Jack Brannon <166420733+Jack-Brannon@users.noreply.github.com>
2026-04-10 13:57:41 +0100

    Dcwl 4654 (#174)
    * DCWL-4654 | unit test coverage increased
    
    * DCWL-4654 | unit test coverage increased

--- test/viewmodels/DutyDefermentStatementForEoriSpec.scala
+++ test/viewmodels/DutyDefermentStatementForEoriSpec.scala
@@ -16,72 +16,164 @@
 
 package viewmodels
 
-import models.DDStatementType.Weekly
+import models.DDStatementType.{DutyDeferment, Excise, ExciseDeferment, Supplementary, Weekly}
 import models.{DutyDefermentStatementFile, DutyDefermentStatementFileMetadata, FileFormat}
 import models.FileRole.DutyDefermentStatement
 import util.SpecBase
 import org.scalatest.matchers.should.Matchers.shouldBe
+import utils.Utils.listOfPastNthMonths
 
 import java.time.LocalDate
 
 class DutyDefermentStatementForEoriSpec extends SpecBase {
 
-  "groups" must {
+  "groups" should {
 
-    "correctly give statements by month which are less than 7 months" in new Setup {
+    "month handling" should {
+      "always return current month plus at least 6 months previous for partial data" in new Setup {
+        override val currentStatements: Seq[DutyDefermentStatementFile] = dutyDefermentStatementFiles
 
-      val currentStatementsTestData: Seq[DutyDefermentStatementFile] = List(
-        DutyDefermentStatementFile("someFilename", "downloadUrl", fileSizeData, metaData01),
-        DutyDefermentStatementFile("someFilename2", "downloadUrl", fileSizeData, metaData02),
-        DutyDefermentStatementFile("someFilename3", "downloadUrl", fileSizeData, metaData03)
-      )
+        result.groups.size shouldBe 7
+      }
+      "always return current month plus at least 6 months previous for no data" in new Setup {
 
-      val requestedStatementsTestData: Seq[DutyDefermentStatementFile] = List(
-        DutyDefermentStatementFile("someFilename", "downloadUrl", fileSizeData, metaData01)
-      )
+        result.groups.size shouldBe 7
+      }
+      "return 8th month when statements are present for 8th month (catching 217 day retention)" in new Setup {
+        override val currentStatements: Seq[DutyDefermentStatementFile] = List(ddStatementFile(metadata07))
 
-      lazy val dutyDefermentStatementsForEori: DutyDefermentStatementsForEori = DutyDefermentStatementsForEori(
-        eoriHistory,
-        currentStatementsTestData,
-        requestedStatementsTestData,
-        LocalDate.now()
-      )
+        result.groups.size       shouldBe 8
+        result.groups.last.month shouldBe todaysDate.minusMonths(7).getMonth.getValue
+      }
+      "remove 7th month when statements are NOT present for 7th month" in new Setup {
 
-      dutyDefermentStatementsForEori.currentStatements.size   shouldBe 3
-      dutyDefermentStatementsForEori.requestedStatements.size shouldBe 1
-      dutyDefermentStatementsForEori.groups.size              shouldBe 7
-      dutyDefermentStatementsForEori.groups.head.periods.size shouldBe 0
-      dutyDefermentStatementsForEori.groups.tail.size         shouldBe 6
+        result.groups.size       shouldBe 7
+        result.groups.last.month shouldBe todaysDate.minusMonths(6).getMonth.getValue
+      }
+      "return months in descending order (latest first)" in new Setup {
+
+        val months: Seq[LocalDate]                = result.groups.map(_.monthAndYear)
+        val expectedMonths: IndexedSeq[LocalDate] = listOfPastNthMonths(endDate, 6)
+
+        months shouldBe expectedMonths
+      }
+      "exclude statements older than 7 months" in new Setup {
+        override val currentStatements: Seq[DutyDefermentStatementFile] =
+          dutyDefermentStatementFiles ++ List(ddStatementFile(metadata07), ddStatementFile(metaData10))
+
+        result.groups.size shouldBe 8
+      }
+      "populate missing months with empty dummy periods" in new Setup {
+
+        result.groups.flatMap(_.periods.flatMap(_.statementFiles)) shouldBe empty
+      }
     }
 
-    "correctly give no statements when dates are more than 7 months" in new Setup {
+    "period grouping" should {
+      "group multiple files with identical metadata into one period" in new Setup {
+        override val currentStatements: Seq[DutyDefermentStatementFile] =
+          List(ddStatementFile(metaData02), ddStatementFile(metaData02))
 
-      val currentStatementsTestData: Seq[DutyDefermentStatementFile] = List(
-        DutyDefermentStatementFile("someFilename3", "downloadUrl", fileSizeData, metaData03)
-      )
+        val month         = ddStatementFile(metaData02).monthAndYear
+        val periods       = result.groups.find(_.monthAndYear == month).value.periods
+        val weeklyperiods = periods.filter(_.defermentStatementType == Weekly)
 
-      val requestedStatementsTestData: Seq[DutyDefermentStatementFile] = List(
-        DutyDefermentStatementFile("someFilename", "downloadUrl", fileSizeData, metaData01)
-      )
+        weeklyperiods.size                                                     shouldBe 4
+        weeklyperiods.count(_.periodIssueNumber == 1)                          shouldBe 1
+        weeklyperiods.find(_.periodIssueNumber == 1).value.statementFiles.size shouldBe 2
+        weeklyperiods.find(_.periodIssueNumber == 2).value.statementFiles      shouldBe empty
+      }
+      "keep separate periods when metadata differs e.g. same month but different week statement" in new Setup {
+        val metaData02Week2 =
+          ddStatementFile(metaData02.copy(periodStartDay = 8, periodEndDay = 15, periodIssueNumber = 2))
 
-      lazy val dutyDefermentStatementsForEori: DutyDefermentStatementsForEori = DutyDefermentStatementsForEori(
-        eoriHistory,
-        currentStatementsTestData,
-        requestedStatementsTestData,
-        LocalDate.now()
-      )
+        override val currentStatements: Seq[DutyDefermentStatementFile] =
+          List(ddStatementFile(metaData02), ddStatementFile(metaData02), metaData02Week2)
 
-      dutyDefermentStatementsForEori.currentStatements.size   shouldBe 1
-      dutyDefermentStatementsForEori.requestedStatements.size shouldBe 1
-      dutyDefermentStatementsForEori.groups.isEmpty           shouldBe false
+        val month         = ddStatementFile(metaData02).monthAndYear
+        val periods       = result.groups.find(_.monthAndYear == month).value.periods
+        val weeklyperiods = periods.filter(_.defermentStatementType == Weekly)
+
+        weeklyperiods.size                                                     shouldBe 4
+        weeklyperiods.count(_.periodIssueNumber == 1)                          shouldBe 1
+        weeklyperiods.find(_.periodIssueNumber == 1).value.statementFiles.size shouldBe 2
+        weeklyperiods.find(_.periodIssueNumber == 2).value.statementFiles.size shouldBe 1
+      }
     }
 
+    "weekly logic"     should {
+      "include all existing weekly periods and add dummy periods for those missing" in new Setup {
+        override val currentStatements: Seq[DutyDefermentStatementFile] =
+          List(ddStatementFile(metaData02))
+
+        val month         = ddStatementFile(metaData02).monthAndYear
+        val periods       = result.groups.find(_.monthAndYear == month).value.periods
+        val weeklyperiods = periods.filter(_.defermentStatementType == Weekly)
+
+        weeklyperiods.size                                                     shouldBe 4
+        weeklyperiods.count(_.periodIssueNumber == 1)                          shouldBe 1
+        weeklyperiods.count(_.periodIssueNumber == 2)                          shouldBe 1
+        weeklyperiods.count(_.periodIssueNumber == 3)                          shouldBe 1
+        weeklyperiods.count(_.periodIssueNumber == 4)                          shouldBe 1
+        weeklyperiods.find(_.periodIssueNumber == 1).value.statementFiles.size shouldBe 1
+        weeklyperiods.find(_.periodIssueNumber == 2).value.statementFiles      shouldBe empty
+        weeklyperiods.find(_.periodIssueNumber == 3).value.statementFiles      shouldBe empty
+        weeklyperiods.find(_.periodIssueNumber == 4).value.statementFiles      shouldBe empty
+      }
+    }
+    "non-weekly logic" should {
+      "include all existing non weekly periods and add dummy periods for those missing" in new Setup {
+        val metaData02Week2 = ddStatementFile(metaData02.copy(defermentStatementType = Excise))
+
+        override val currentStatements: Seq[DutyDefermentStatementFile] = List(metaData02Week2)
+
+        val month   = ddStatementFile(metaData02).monthAndYear
+        val periods = result.groups.find(_.monthAndYear == month).value.periods
+
+        periods.find(_.defermentStatementType == Excise).value.statementFiles.size     shouldBe 1
+        periods.find(_.defermentStatementType == Supplementary).value.statementFiles   shouldBe empty
+        periods.find(_.defermentStatementType == ExciseDeferment).value.statementFiles shouldBe empty
+        periods.find(_.defermentStatementType == DutyDeferment).value.statementFiles   shouldBe empty
+        periods.filter(_.defermentStatementType == Weekly).size                        shouldBe 4
+
+      }
+    }
+    "cutoff behaviour" should {
+      "not add missing periods before cutoff date" in new Setup {
+        override val endDate = todaysDate.withDayOfMonth(1)
+
+        val group = result.groups.find(_.monthAndYear == endDate).value
+        group.periods shouldBe empty
+      }
+      "add missing periods after cutoff date" in new Setup {
+        override val currentStatements: Seq[DutyDefermentStatementFile] = List(ddStatementFile(metaData02))
+
+        val month   = ddStatementFile(metaData02).monthAndYear
+        val periods = result.groups.find(_.monthAndYear == month).value.periods
+
+        periods.size shouldBe 8
+      }
+    }
   }
 
   trait Setup {
+
+    val endDate = todaysDate
+
+    val currentStatements: Seq[DutyDefermentStatementFile]   = Seq.empty
+    val requestedStatements: Seq[DutyDefermentStatementFile] = Seq.empty
+
+    lazy val result = DutyDefermentStatementsForEori(
+      eoriHistory,
+      currentStatements,
+      requestedStatements,
+      endDate
+    )
+
     val startDate01 = todaysDate.minusMonths(1)
     val startDate02 = todaysDate.minusMonths(2)
-    val startDate03 = todaysDate.minusMonths(periodStartMonth10)
+    val startDate10 = todaysDate.minusMonths(10)
+    val startDate07 = todaysDate.minusMonths(7)
 
     val metaData01 = DutyDefermentStatementFileMetadata(
       startDate01.getYear,
@@ -117,12 +209,12 @@ class DutyDefermentStatementForEoriSpec extends SpecBase {
       None
     )
 
-    val metaData03 = DutyDefermentStatementFileMetadata(
-      startDate03.getYear,
-      startDate03.getMonthValue,
+    val metaData10 = DutyDefermentStatementFileMetadata(
+      startDate10.getYear,
+      startDate10.getMonthValue,
       1,
-      startDate03.getYear,
-      startDate03.getMonthValue,
+      startDate10.getYear,
+      startDate10.getMonthValue,
       periodStartDay1,
       periodIssueNumber1,
       FileFormat.Csv,
@@ -134,5 +226,24 @@ class DutyDefermentStatementForEoriSpec extends SpecBase {
       None
     )
 
+    def ddStatementFile(metadata: DutyDefermentStatementFileMetadata) =
+      DutyDefermentStatementFile("someFilename", "downloadUrl", fileSizeData, metadata)
+
+    val metadata07 = DutyDefermentStatementFileMetadata(
+      startDate07.getYear,
+      startDate07.getMonthValue,
+      periodStartDay1,
+      startDate07.getYear,
+      startDate07.getMonthValue,
+      periodEndDay8,
+      periodIssueNumber1,
+      FileFormat.Csv,
+      DutyDefermentStatement,
+      Excise,
+      None,
+      None,
+      "1234",
+      None
+    )
   }
 }
