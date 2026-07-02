883701a9a175938e0a98ff2af659084ec4b2f534
AdamCoops <adam.cooper2@digital.hmrc.gov.uk>
2026-07-02 15:15:34 +0100

    DCWL-4622-update play ver

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.8.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.9.0",
     "org.typelevel"     %% "cats-core"                             % "2.13.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion
   )
